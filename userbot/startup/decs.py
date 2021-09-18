f5fcffd0e28a77eda5e1914df5a628bbe4b0d6b9
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-18 22:36:56 +0530

    Add files via upload

--- userbot/startup/decs.py
+++ userbot/startup/decs.py
@@ -5,15 +5,13 @@ from pathlib import Path
 from telethon import events
 CMD_LIST = {}
 LOAD_PLUG = {}
+from .session import L2, L3, L4, L5, Legend
+bot = Legend
+
 BL_CHAT = "-1001500629429"
 SUDO_USERS = os.environ.get("SUDO_USERS", None)
 HANDLER = os.environ.get("COMMAND_HAND_LER", None)
-SUDO_HANDLER = os.environ.get("SUDO_COMMAND_HAND_LER", ".") or "."
-
-from .session import L2, L3, L4, L5, Legend
-from ..config import Config
-bot = Legend
-
+SUDO_HANDLER = os.environ.get("SUDO_HANDLER", ".") or "."
 def legend_cmd(
     pattern: str = None,
     allow_sudo: bool = True,
@@ -31,31 +29,26 @@ def legend_cmd(
     if "disable_edited" in args:
         del args["disable_edited"]
 
-    args["blacklist_chats"] = True
-    black_list_chats = list(Config.BL_CHAT)
-    if len(black_list_chats) > 0:
-        args["chats"] = black_list_chats
-
     if pattern is not None:
-        global LEGEND_reg
+        global legend_reg
         global sudo_reg
         if (
             pattern.startswith(r"\#")
             or not pattern.startswith(r"\#")
             and pattern.startswith(r"^")
         ):
-            LEGEND_reg = sudo_reg = re.compile(pattern)
+            legend_reg = sudo_reg = re.compile(pattern)
         else:
-            LEGEND_ = "\\" + Config.HANDLER
-            sudo_ = "\\" + Config.SUDO_HANDLER
-            LEGEND_reg = re.compile(LEGEND_ + pattern)
+            legend_ = "\\" + HANDLER
+            sudo_ = "\\" + SUDO_HANDLER
+            legend_reg = re.compile(legend_ + pattern)
             sudo_reg = re.compile(sudo_ + pattern)
             if command is not None:
-                cmd1 = LEGEND_ + command
+                cmd1 = legend_ + command
                 cmd2 = sudo_ + command
             else:
                 cmd1 = (
-                    (LEGEND_ + pattern).replace("$", "").replace("\\", "").replace("^", "")
+                    (legend_ + pattern).replace("$", "").replace("\\", "").replace("^", "")
                 )
                 cmd2 = (
                     (sudo_ + pattern).replace("$", "").replace("\\", "").replace("^", "")
@@ -68,18 +61,18 @@ def legend_cmd(
 
     def decorator(func):
         if not disable_edited:
-            bot.add_event_handler(func, events.MessageEdited(**args, outgoing=True, pattern=LEGEND_reg))
-        bot.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=LEGEND_reg))
+            bot.add_event_handler(func, events.MessageEdited(**args, outgoing=True, pattern=legend_reg))
+        bot.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=legend_reg))
         if allow_sudo:
-            bot.add_event_handler(func, events.NewMessage(**args, from_users=list(Config.SUDO_USERS), pattern=sudo_reg))
+            bot.add_event_handler(func, events.NewMessage(**args, from_users=list(SUDO_USERS), pattern=sudo_reg))
         if L2:
-            L2.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=LEGEND_reg))
+            L2.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=legend_reg))
         if L3:
-            L3.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=LEGEND_reg))
+            L3.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=legend_reg))
         if L4:
-            L4.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=LEGEND_reg))
+            L4.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=legend_reg))
         if L5:
-            L5.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=LEGEND_reg))
+            L5.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=legend_reg))
         try:
             LOAD_PLUG[file_test].append(func)
         except Exception:
@@ -87,20 +80,3 @@ def legend_cmd(
         return func
 
     return decorator
-
-def legend_handler(**args):
-    def decorator(func):
-        async def wrapper(event):
-            await func(event)
-        bot.add_event_handler(wrapper, events.NewMessage(**args))
-        if L2:
-            L2.add_event_handler(wrapper, events.NewMessage(**args))
-        if L3:
-            L3.add_event_handler(wrapper, events.NewMessage(**args))
-        if L4:
-            L4.add_event_handler(wrapper, events.NewMessage(**args))
-        if L5:
-            L5.add_event_handler(wrapper, events.NewMessage(**args))
-        return wrapper
-
-    return decorater
