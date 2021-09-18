6554072e8b34b3062cd3bffd182ef624d0360be6
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-18 22:26:16 +0530

    Update __init__.py

--- userbot/startup/__init__.py
+++ userbot/startup/__init__.py
@@ -3,5 +3,5 @@ from .decs import legend_cmd, legend_handler
 from .session import Legend, L2, L3, L4, L5, LegendBot
 import telethon.utils
 from userbot.Config import Config
-
+BL_CHAT = "-1001500629429"
 
