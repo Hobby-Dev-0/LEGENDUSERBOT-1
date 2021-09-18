ec32b5db196035e85ee2a095e22e41995ab23788
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-18 22:25:32 +0530

    Update __init__.py

--- userbot/startup/__init__.py
+++ userbot/startup/__init__.py
@@ -1,5 +1,5 @@
 
-from .decs import legend_cmd
+from .decs import legend_cmd, legend_handler
 from .session import Legend, L2, L3, L4, L5, LegendBot
 import telethon.utils
 from userbot.Config import Config
