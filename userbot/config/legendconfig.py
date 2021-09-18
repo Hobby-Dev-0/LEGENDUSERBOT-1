5af5885772c41d32e69c7d34722cb121ccaeb6ce
†hê~Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-09-18 21:47:44 +0530

    Update legendconfig.py

--- userbot/config/legendconfig.py
+++ userbot/config/legendconfig.py
@@ -153,123 +153,96 @@ class Config(object):
     )
         # chat ids or usernames, it is recommended to use chat ids,
         # providing usernames means an additional overhead for the user
-        CHATS_TO_MONITOR_FOR_ANTI_FLOOD = []
-        # Get your own API key from https://www.remove.bg/ or
-        # feel free to use http://telegram.dog/Remove_BGBot
-        REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-        # Set to True if you want to block users that are spamming your PMs.
-        SLAP_USERNAME = os.environ.get("SLAP_USERNAME", None)
-        GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
-        GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
-        NO_P_M_SPAM = bool(os.environ.get("NO_P_M_SPAM", True))
+
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+        # Set to True if you want to block users that are spamming your PMs
+    SLAP_USERNAME = os.environ.get("SLAP_USERNAME", None)
+    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
+    GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
+    NO_P_M_SPAM = bool(os.environ.get("NO_P_M_SPAM", True))
         # define "spam" in PMs
-        NO_SONGS = bool(os.environ.get("NO_SONGS", False))
-        MAX_FLOOD_IN_PM = int(os.environ.get("MAX_FLOOD_IN_PM", 5))
+    NO_SONGS = bool(os.environ.get("NO_SONGS", False))
+    MAX_FLOOD_IN_PM = int(os.environ.get("MAX_FLOOD_IN_PM", 5))
         #pm log
-        PM_LOG_GRP_ID = os.environ.get("PM_LOG_GRP_ID", None)
+    PM_LOG_GRP_ID = os.environ.get("PM_LOG_GRP_ID", None)
         # set to True if you want to log PMs to your PM_LOGGR_BOT_API_ID
-        NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", True))
+    NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", True))
         #heroku 
-        HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-        HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
         # send .get_id in any channel to forward all your NEW PMs to this group
-        PRIVATE_GROUP_BOT_API_ID = os.environ.get("PRIVATE_GROUP_BOT_API_ID", None)
-        if PRIVATE_GROUP_BOT_API_ID:
-            PRIVATE_GROUP_BOT_API_ID = int(PRIVATE_GROUP_BOT_API_ID)
+    PRIVATE_GROUP_BOT_API_ID = os.environ.get("PRIVATE_GROUP_BOT_API_ID", None)
+     if PRIVATE_GROUP_BOT_API_ID:
+         PRIVATE_GROUP_BOT_API_ID = int(PRIVATE_GROUP_BOT_API_ID)
         # send .get_id in your private channel to forward all your Private messages
 
-        TAG_LOGGER = os.environ.get("TAG_LOGGER", None)
-        if TAG_LOGGER: TAG_LOGGER = int(TAG_LOGGER)
+     TAG_LOGGER = os.environ.get("TAG_LOGGER", None)
+     if TAG_LOGGER: TAG_LOGGER = int(TAG_LOGGER)
 
         #Tag LOGGER
 
-        PM_LOGGR_BOT_API_ID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
-        if PM_LOGGR_BOT_API_ID: PM_LOGGR_BOT_API_ID = int(PM_LOGGR_BOT_API_ID)
+     PM_LOGGR_BOT_API_ID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
+     if PM_LOGGR_BOT_API_ID: PM_LOGGR_BOT_API_ID = int(PM_LOGGR_BOT_API_ID)
         # For Databases
         # can be None in which case plugins requiring
         # DataBase would not work
-        DB_URI = os.environ.get("DATABASE_URL", None)
-        # number of rows of buttons to be displayed in .legend command
-        BUTTONS_IN_HELP = int(os.environ.get("NO_OF_BUTTONS", 7))
+     DB_URI = os.environ.get("DATABASE_URL", None)
+    # number of rows of buttons to be displayed in .legend command
+     BUTTONS_IN_HELP = int(os.environ.get("NO_OF_BUTTONS", 7))
         #open load
-        OPEN_LOAD_LOGIN = os.environ.get("OPEN_LOAD_LOGIN", None)
-        OPEN_LOAD_KEY = os.environ.get("OPEN_LOAD_KEY", None)
+     OPEN_LOAD_LOGIN = os.environ.get("OPEN_LOAD_LOGIN", None)
+     OPEN_LOAD_KEY = os.environ.get("OPEN_LOAD_KEY", None)
         # number of colums of buttons to be displayed in .legend command
-        NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD = int(os.environ.get("NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD", 2))
+     NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD = int(os.environ.get("NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD", 2))
         # emoji to be displayed  in help .legend
-        EMOJI_IN_HELP1 = os.environ.get("EMOJI_IN_HELP1", "⚜️ ")
-        EMOJI_IN_HELP2 = os.environ.get("EMOJI_IN_HELP2", "🌹 ")
+     EMOJI_IN_HELP1 = os.environ.get("EMOJI_IN_HELP1", "⚜️ ")
+     EMOJI_IN_HELP2 = os.environ.get("EMOJI_IN_HELP2", "🌹 ")
         # specify command handler that should be used for the plugins
         # this should be a valid "regex" pattern
-        COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", r"\.")
-        HANDLER = os.environ.get("COMMAND_HAND_LER", r"\.")
-        #custom animation to kang plugin
-        CUSTOM_STICKER_PACK_NAME = os.environ.get("CUSTOM_STICKER_PACK_NAME", None)
+     COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", r"\.")
+     HANDLER = os.environ.get("COMMAND_HAND_LER", r"\.")
+      #custom animation to kang plugin
+     CUSTOM_STICKER_PACK_NAME = os.environ.get("CUSTOM_STICKER_PACK_NAME", None)
         # specify list of users allowed to use bot
         # WARNING: be careful who you grant access to your bot.
         # malicious users could do ".exec rm -rf /*"
-        SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
-        # VeryStream only supports video formats
-        VERY_STREAM_LOGIN = os.environ.get("VERY_STREAM_LOGIN", None)
-        VERY_STREAM_KEY = os.environ.get("VERY_STREAM_KEY", None)
-        GROUP_REG_SED_EX_BOT_S = os.environ.get("GROUP_REG_SED_EX_BOT_S", r"(regex|moku|BananaButler_|rgx|l4mR)bot")
-        TEMP_DIR = os.environ.get("TEMP_DIR", None)
-        CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-        watermark_path = os.environ.get("watermark_path", None)
-        #Google Chrome Stuff
-        CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/app/.chromedriver/bin/chromedriver")
-        GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
-        # Google Drive ()
-        G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-        G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-        GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-        AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
-        if AUTH_TOKEN_DATA != None:
-            os.makedirs(TMP_DOWNLOAD_DIRECTORY)
-            t_file = open(TMP_DOWNLOAD_DIRECTORY+"auth_token.txt","w")
-            t_file.write(AUTH_TOKEN_DATA)
-            t_file.close()
-
-        YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
-        GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-        #MongoDB
-        MONGO_URI = os.environ.get("MONGO_URI", None)
-        #alive
-        ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
-        PM_PIC = os.environ.get("PM_PIC", None)
-        AWAKE_PIC = os.environ.get("AWAKE_PIC", None)
-        HELP_PIC = os.environ.get("OP_PIC", None)
-        ALIVE_MSG = os.environ.get("ALIVE_MSG", None)
-        PM_MSG = os.environ.get("PM_MSG", None)
-        INSTANT_BLOCK = os.environ.get("INSTANT_BLOCK", "DISABLE")
-        YOUR_GROUP = os.environ.get("YOUR_GROUP", "@Legend_Userbot")
-        YOUR_CHANNEL = os.environ.get("YOUR_CHANNEL", "@Its_LegendBot.")
-        BOT_PIC = os.environ.get("ALIVE_PIC", None)
+     SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
+        
+     ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
+     PM_PIC = os.environ.get("PM_PIC", None)
+     AWAKE_PIC = os.environ.get("AWAKE_PIC", None)
+     HELP_PIC = os.environ.get("OP_PIC", None)
+     ALIVE_MSG = os.environ.get("ALIVE_MSG", None)
+     PM_MSG = os.environ.get("PM_MSG", None)
+     INSTANT_BLOCK = os.environ.get("INSTANT_BLOCK", "DISABLE")
+     YOUR_GROUP = os.environ.get("YOUR_GROUP", "@Legend_Userbot")
+     YOUR_CHANNEL = os.environ.get("YOUR_CHANNEL", "@Its_LegendBot.")
+     BOT_PIC = os.environ.get("ALIVE_PIC", None)
         #auto bio
-        BIO_MSG = os.environ.get("ALIVE_MSG", None)
+     BIO_MSG = os.environ.get("ALIVE_MSG", None)
         #Lydia API
-        LYDIA_API = os.environ.get("LYDIA_API",None)
-        PLUGIN_CHANNEL = os.environ.get("PLUGIN_CHANNEL",None)
-        UPSTREAM_REPO = os.environ.get(
-            "UPSTREAM_REPO", "https://github.com/LEGEND-OS/LEGENDBOT"
-        )
-        BOT_MODE = os.environ.get("BOT_MODE", "ON")
-        ABUSE = os.environ.get("ABUSE", None)
-        BOTLOG_CHATID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
-        ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
-        BOY_OR_GIRL = os.environ.get("BOY_OR_GIRL", "BOY")
-        BOT_TRIGGER = os.environ.get("BOT_TRIGGER", "^/")
-        BOTMODE_LOG = int(os.environ.get("BOTMODE_LOG", False))
-        BOT_TOKEN = os.environ.get("BOT_TOKEN", None) 
-        BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
-        FORCE_SUB = os.environ.get("FORCE_SUB", None)
-        FORCE_CHANNEL_UN = os.environ.get("FORCE_CHANNEL_UN", None)
-        LOGGER_ID = os.environ.get("LOGGER_ID", None)
-        if LOGGER_ID:
-            LOGGER_ID = int(LOGGER_ID)
-        FORCE_CHANNEL_ID = int(os.environ.get("FORCE_CHANNEL_ID", False))
-        EXTRA_LEGENDBOT = os.environ.get("EXTRA_LEGENDBOT", -1001221881562)
-        PM_DATA = os.environ.get("PM_DATA", "ENABLE")
+     LYDIA_API = os.environ.get("LYDIA_API",None)
+     PLUGIN_CHANNEL = os.environ.get("PLUGIN_CHANNEL",None)
+     UPSTREAM_REPO = os.environ.get(
+         "UPSTREAM_REPO", "https://github.com/LEGEND-OS/LEGENDBOT"
+     )
+     BOT_MODE = os.environ.get("BOT_MODE", "ON")
+     ABUSE = os.environ.get("ABUSE", None)
+     BOTLOG_CHATID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
+     ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
+     BOY_OR_GIRL = os.environ.get("BOY_OR_GIRL", "BOY")
+     BOT_TRIGGER = os.environ.get("BOT_TRIGGER", "^/")
+     BOTMODE_LOG = int(os.environ.get("BOTMODE_LOG", False))
+     BOT_TOKEN = os.environ.get("BOT_TOKEN", None) 
+     BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
+     FORCE_SUB = os.environ.get("FORCE_SUB", None)
+     FORCE_CHANNEL_UN = os.environ.get("FORCE_CHANNEL_UN", None)
+     LOGGER_ID = os.environ.get("LOGGER_ID", None)
+     if LOGGER_ID:
+         LOGGER_ID = int(LOGGER_ID)
+     FORCE_CHANNEL_ID = int(os.environ.get("FORCE_CHANNEL_ID", False))
+     EXTRA_LEGENDBOT = os.environ.get("EXTRA_LEGENDBOT", -1001221881562)
+     PM_DATA = os.environ.get("PM_DATA", "ENABLE")
 
 class Production(Config):
     LOGGER = False
