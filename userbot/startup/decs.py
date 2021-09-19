de1063404ca0bc594019e1e7e511bbcbe6330314
ForbiddenLinkers <87512153+ForbiddenLinkers@users.noreply.github.com>
2021-09-19 13:49:46 +0530

    decs

--- userbot/startup/decs.py
+++ userbot/startup/decs.py
@@ -60,79 +60,6 @@ def legend_cmd(
 
 
     def decorator(func):
-        async def wrapper(check):
-            if check.edit_date and check.is_channel and not check.is_group:
-                # Messages sent in channels can be edited by other users.
-                # Ignore edits that take place in channels.
-                return
-            if group_only and not check.is_group:
-                await check.respond("`Are you sure this is a group?`")
-                return
-            if check.via_bot_id and not insecure and check.out:
-                # Ignore outgoing messages via inline bots for security reasons
-                return
-
-            try:
-                await func(check)
-            except events.StopPropagation:
-                raise events.StopPropagation
-            # This is a gay exception and must be passed out. So that it doesnt spam chats
-            except KeyboardInterrupt:
-                pass
-            except BaseException as e:
-                LOGS.exception(e)  # Log the error in console
-                # Check if we have to disable error logging message.
-                if not disable_errors:
-                    date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
-                    text = "**ELGEND BOT ERROR REPORT!**\n"
-                    link = "[https://t.me/Legend_Userbot](Userbot Support Chat)"
-                    text += "If you want to report it, "
-                    text += f"just forward this message to {link}.\n"
-                    text += "I won't log anything except the fact of error and date.\n"
-
-                    ftext = "\nDisclaimer:\nThis file is uploaded ONLY here, "
-                    ftext += "we logged only fact of error and date, "
-                    ftext += "we respect your privacy. "
-                    ftext += "You may not report this error if you have "
-                    ftext += "any confidential data here. No one will see your data "
-                    ftext += "if you choose not to do so.\n\n"
-                    ftext += "--------BEGIN USERBOT TRACEBACK LOG--------"
-                    ftext += "\nDate: " + date
-                    ftext += "\nGroup ID: " + str(check.chat_id)
-                    ftext += "\nSender ID: " + str(check.sender_id)
-                    ftext += "\n\nEvent Trigger:\n"
-                    ftext += str(check.text)
-                    ftext += "\n\nTraceback info:\n"
-                    ftext += str(format_exc())
-                    ftext += "\n\nError text:\n"
-                    ftext += str(sys.exc_info()[1])
-                    ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
-
-                    command = 'git log --pretty=format:"%an: %s" -5'
-
-                    ftext += "\n\n\nLast 5 commits:\n"
-
-                    process = await asyncsubshell(
-                        command, stdout=asyncsub.PIPE, stderr=asyncsub.PIPE
-                    )
-                    stdout, stderr = await process.communicate()
-                    result = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-                    ftext += result
-
-                    with open("error.log", "w+") as output_file:
-                        output_file.write(ftext)
-
-                    if BOTLOG:
-                        await check.client.send_file(
-                            check.chat_id, "error.log", caption=text
-                        )
-
-                    remove("error.log")
-            else:
-                pass
-            
         if not disable_edited:
             bot.add_event_handler(func, events.MessageEdited(**args, outgoing=True, pattern=legend_reg))
         bot.add_event_handler(func, events.NewMessage(**args, outgoing=True, pattern=legend_reg))
@@ -150,8 +77,8 @@ def legend_cmd(
             LOAD_PLUG[file_test].append(func)
         except Exception:
             LOAD_PLUG.update({file_test: [func]})
-        return wrapper
-        
+        return func
+
     return decorator
 
 def legend_handler(**args):
