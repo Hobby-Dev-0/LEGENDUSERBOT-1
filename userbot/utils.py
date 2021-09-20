e58b2ee04c86980b0efffe94e216f9625b6561f0
ForbiddenLinkers <87512153+ForbiddenLinkers@users.noreply.github.com>
2021-09-20 09:06:23 +0530

    utility

--- userbot/utils.py
+++ userbot/utils.py
@@ -798,8 +798,8 @@ def start_assistant(shortname):
         import sys
         from pathlib import Path
 
-        path = Path(f"userbot/plugins/assistant/{shortname}.py")
-        name = "userbot.plugins.assistant.{}".format(shortname)
+        path = Path(f"userbot/plugins/Assistant/{shortname}.py")
+        name = "userbot.plugins.Assistant.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
