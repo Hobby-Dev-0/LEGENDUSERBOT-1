85074f2443edb25b7d557c4a81312118ab4d664e
Hobby-Dev // <72003733+Hobby-Dev-0@users.noreply.github.com>
2021-09-21 08:35:12 +0530

    Redis Moved

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,6 +5,7 @@ from telethon.sessions import StringSession
 from telethon import TelegramClient
 from userbot.Config.Config import Config
 from var import Var
+from .startup.connect import *
 from .startup import *
 from .k import *
 BOTLOG = True
@@ -67,23 +68,6 @@ try:
 except:
     HEROKU_APP = None
 
-import os
-REDIS_KEY = os.environ.get("REDIS_KEY", None)
-REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
-if REDIS_KEY and REDIS_PASSWORD:
-    import redis
-    redis_info = REDIS_KEY.split(":")
-    dB = redis.StrictRedis(
-        host=redis_info[0],
-        port=redis_info[1],
-        password=REDIS_PASSWORD,
-        charset="utf-8",
-        decode_responses=True,
-    )
-    print("Connected To Redis")
-else:
-    print("Could Not Connnect To Redis Check Your Redis Key And Pass if facing issue come to support group Quitting!")
-    sys.exit(1)
 
 
 # Setting Up CloudMail.ru and MEGA.nz extractor binaries,
